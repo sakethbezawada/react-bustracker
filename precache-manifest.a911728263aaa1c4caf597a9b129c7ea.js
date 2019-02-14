5011647f2c9a59ba07b2b0917de21ff496875334
Prathap Kathi <prathap.kathi@valuelabs.com>
2019-02-14 11:17:14 +0530

    Updates

---
+++ precache-manifest.a911728263aaa1c4caf597a9b129c7ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "da25ae6cd06c27810583",
+    "url": "/static/css/main.e5eb2810.chunk.css"
+  },
+  {
+    "revision": "da25ae6cd06c27810583",
+    "url": "/static/js/main.da25ae6c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fb40fa3cd2039f719eac35da150cd686",
+    "url": "/index.html"
+  }
+];
