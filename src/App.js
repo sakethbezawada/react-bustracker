bf7d2d93c8b573a44259c72d194269b9a9affbf1
Prathap Kathi <prathap.kathi@valuelabs.com>
2019-02-14 12:04:53 +0530

    Added browser rounter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import {
+  BrowserRouter 
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+     
+
+<BrowserRouter
+basename="/">
+<div className="App">
         <p>Hello Bus Tracker</p>
       </div>
+</BrowserRouter>
     );
   }
 }
